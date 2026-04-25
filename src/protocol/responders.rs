35865fc1e27d62f4b1b2501f3fa9442cffa89813
hrithikesh026 <hrithikesh.vm@juspay.in>
2026-04-25 19:33:10 +0530

    remove unwanted yield

--- src/protocol/responders.rs
+++ src/protocol/responders.rs
@@ -454,7 +454,6 @@ pub async fn respond_to_caller(
   _trace!(inner, "Send frame result {:?}", send_result);
   command.respond_to_router(inner, RouterResponse::Continue).await;
   _trace!(inner, "returning from respond_to_caller");
-  tokio::task::yield_now().await;
   Ok(())
 }
 
